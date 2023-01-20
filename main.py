0a2d3101705e5f5baf3f904796233d37b7816f95
JaAy27 <jnax27@hotmail.com>
2023-01-20 18:44:07 -0500

    Add date

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from fpdf import FPDF
 from pathlib import Path
 
 filepaths = glob.glob("Invoices/*.xlsx")
-print(filepaths)
 
+# Read Excel file, create PDF, Write Title
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
     pdf = FPDF(orientation="P", unit="mm", format="A4")
@@ -14,10 +14,16 @@ for filepath in filepaths:
 
     # Get the invoice number
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt="Invoice nr.")
+    pdf.cell(w=50, h=8, txt=f"Invoice #{invoice_nr}", ln=1)
+
+    pdf.set_font(family="Times", size=16, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
+
+
     pdf.output(f"PDFs/{filename}.pdf")
 
 
